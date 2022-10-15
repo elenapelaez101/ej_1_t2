967c19c15469d287b3988c948c74475f33d34a6c
elenapelaez101 <elena.pelaezsanchez@usp.ceu.es>
2022-10-15 13:39:56 +0200

    cuadrante

--- relleno.py
+++ relleno.py
@@ -6,3 +6,21 @@ class Punto:
     def __str__(self):
         formato = "({0},{1})".format(self.x,self.y)
         return formato
+    def cuadrante(self):
+        cuadrante = None
+        if self.x > 0 and self.y> 0:
+            cuadrante = "Primer cuadrante"
+        elif self.x > 0 and self.y< 0:
+            cuadrante = "Segundo cuadrante"
+        elif self.x < 0 and self.y< 0:
+            cuadrante = "Tercer cuadrante"
+        elif self.x < 0 and self.y > 0:
+            cuadrante = "Cuarto cuadrante"
+        elif self.x == 0 and self.y != 0:
+            cuadrante = "Sobre el eje y"
+        elif self.x != 0 and self.y == 0:
+            cuadrante = "Sobre el eje x"
+        elif self.x == 0 and self.y == 0:
+            cuadrante = "Sobre el origen"
+        return cuadrante
+    
